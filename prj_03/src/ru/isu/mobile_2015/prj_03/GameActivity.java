9b459c976a70b89f6586dbfa7d2ef5738ec196d3
Sergey Reymerov <sergeyreym@gmail.com>
2015-11-03 16:42:28 +0800

    comments to game

--- prj_03/src/ru/isu/mobile_2015/prj_03/GameActivity.java
+++ prj_03/src/ru/isu/mobile_2015/prj_03/GameActivity.java
@@ -4,12 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class GameActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // в основной активности в качестве единственного view на ней создзаем и устанваливаем GameView
         setContentView(new GameView(this));
     }
 }
--- prj_03/src/ru/isu/mobile_2015/prj_03/GameThread.java
+++ prj_03/src/ru/isu/mobile_2015/prj_03/GameThread.java
@@ -5,29 +5,41 @@ import android.graphics.Color;
 import android.graphics.Paint;
 
 /**
- * Created by sergey on 27/10/15.
+ * Класс для потока основного цикла игры
  */
 public class GameThread extends Thread {
     GameView gameView;
+
+    /**
+     * Конструктор потока основного цикла просто запоминает GameView, из которого он был создан
+     * @param  gameView
+     */
     public GameThread(GameView gameView) {
         this.gameView = gameView;
     }
 
     @Override
     public void run() {
+        // в цикле будем постоянно получать canvas (холст) для рисования, поэтому объявим эту переменную тут
         Canvas canvas;
         while(true) {
+            // захват и блокировка холста
             canvas = gameView.getHolder().lockCanvas();
 
+            // очищаем холст
             canvas.drawRect(0,0, canvas.getWidth(), canvas.getHeight(), new Paint());
 
+            // вызываем все необходимые трансформации
             gameView.transform();
+            // вызываем все необходимые отрисовки (для отрисовки передаем холст)
             gameView.render(canvas);
 
+            // отпускаем и "рисуем" холст на GameView
             gameView.getHolder().unlockCanvasAndPost(canvas);
 
+            // делаем паусу в 1000/50 секунды
             try {
-                this.sleep(1000 / 50);
+                Thread.sleep(1000 / 50);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
