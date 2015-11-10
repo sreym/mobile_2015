a8cbd817907662020e4fd415e398d5ff200cccad
Sergey Reymerov <sergeyreym@gmail.com>
2015-11-10 17:34:51 +0800

    Refactoring

--- prj_04/src/com/example/prj_04/AuthActivity.java
+++ prj_04/src/com/example/prj_04/AuthActivity.java
@@ -23,29 +23,17 @@ public class AuthActivity extends Activity {
 
         webView = (WebView)findViewById(R.id.webView);
 
-        webView.loadUrl("https://oauth.vk.com/authorize" +
-                "?client_id=5141808" +
-                "&scope=2097150" +
-                "&redirect_uri=https://oauth.vk.com/blank.html" +
-                "&display=mobile" +
-                "&v=5.40" +
-                "&response_type=token");
+        webView.loadUrl(VK.getAuthUrl("5141808", "2097150"));
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-
-                Matcher matcher = Pattern.compile(
-                        "access_token=([0-9a-f]+)"
-                ).matcher(url);
-
-                if (matcher.find()) {
-                    String access_token = matcher.group(1);
+                String token = VK.findAccessTokenInURL(url);
+                if (token != null) {
                     Intent intent = new Intent();
-                    intent.putExtra("access_token", access_token);
+                    intent.putExtra("access_token", token);
                     AuthActivity.this.setResult(ACCESS_TOKEN_TAKEN, intent);
-
                     AuthActivity.this.finish();
                 }
             }
--- prj_04/src/com/example/prj_04/FriendsListActivity.java
+++ prj_04/src/com/example/prj_04/FriendsListActivity.java
@@ -8,26 +8,18 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class MyActivity extends Activity {
+public class FriendsListActivity extends Activity {
     public static final int ACCESS_TOKEN_REQEST = 1;
 
     ListView listView;
-    String access_token;
+    VK vk;
 
     AsyncTask<Void,Void,JSONArray> updateFriendsList = new AsyncTask<Void,Void,JSONArray>() {
         @Override
         protected JSONArray doInBackground(Void... params) {
             try {
-                JSONArray friends = callVK("friends.get", "fields=nickname")
+                JSONArray friends = vk.callFunction("friends.get", "fields=nickname")
                         .getJSONArray("response");
                 return friends;
             } catch (JSONException e) {
@@ -50,48 +42,13 @@ public class MyActivity extends Activity {
             }
 
             ArrayAdapter<String> adapter = new ArrayAdapter(
-                    MyActivity.this,
+                    FriendsListActivity.this,
                     android.R.layout.simple_list_item_1,
                     friendsNames);
 
             listView.setAdapter(adapter);
         }
     };
-
-    // http://pastebin.com/xBea9zhK
-    JSONObject callVK(String method, String params) {
-        try {
-            if (params.length() > 0 && params.charAt(params.length() - 1) != '&') params += '&';
-
-            URL url = new URL("https://api.vk.com/method/"+method+"?"+params+"access_token=" + access_token);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-
-                char[] tl = new char[8 * 1024];
-                while(true) {
-                    int len = in.read(tl);
-                    if (len > 0) {
-                        sb.append(tl, 0, len);
-                    } else {
-                        break;
-                    }
-                }
-
-                return new JSONObject(sb.toString());
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } finally {
-                urlConnection.disconnect();
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 
     /**
      * Called when the activity is first created.
@@ -115,7 +72,7 @@ public class MyActivity extends Activity {
         switch(requestCode) {
             case ACCESS_TOKEN_REQEST:
                 if (resultCode == AuthActivity.ACCESS_TOKEN_TAKEN) {
-                    access_token = data.getStringExtra("access_token");
+                    vk = new VK(data.getStringExtra("access_token"));
                     updateFriendsList.execute();
                 }
                 break;
